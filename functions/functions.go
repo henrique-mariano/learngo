7a4798666ab4ecc905488c2567132936bcd60f19
henrique-mariano <henrique_mariano@aluno.unb.br>
2019-05-03 13:19:56 -0300

    Utilizando pacotes

--- functions/functions.go
+++ functions/functions.go
@@ -1,4 +1,4 @@
-package main
+package functions
 
 import ( //importação consignada
 	"fmt"
@@ -7,29 +7,29 @@ import ( //importação consignada
 	"time"
 )
 
-func printRand() {
+func PrintRand() {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	fmt.Println("Meu número favorito é", r.Int31())
 }
 
-func printProblems() {
+func PrintProblems() {
 	fmt.Printf("Agora você tem %g problemas.\n", math.Sqrt(7)) //Printf para tratar as diretivas
 }
 
-func exportedNames() {
+func ExportedNames() {
 	fmt.Println("Pi:", math.Pi) //nomes exportados começãm com letra maiúscula como Pi e E que são exportados do pacote math
 	fmt.Println("E: ", math.E)
 }
 
-func add(x, y int) int { //Os tipos vem após o nome da variável, isso torna mais legível quanto o que a função faz
+func Add(x, y int) int { //Os tipos vem após o nome da variável, isso torna mais legível quanto o que a função faz
 	return x + y //Quando os tipos consecutivos da função possuem o mesmo tipo, você pode omitir todos os tipos menos o do último
 } //lê-se da esquerda para direita
 
-func swap(x, y string) (string, string) { //uma função pode retornar qualquer número de resultados
+func Swap(x, y string) (string, string) { //uma função pode retornar qualquer número de resultados
 	return y, x
 }
 
-func split(sum int) (x, y int) { //os valores de retorno podem ser nomeados e agirem como variáveis,
+func Split(sum int) (x, y int) { //os valores de retorno podem ser nomeados e agirem como variáveis,
 	x = sum * 4 / 9 //o return sem arg é conhecido como retorno "limpo", retorna os valores atuais dos resultados
 	y = sum - x     //deve-se ser usado apenas em funções curtas, pois elas podem prejudicar a legibilidade em funções longas
 	return
--- learn.go
+++ learn.go
@@ -2,30 +2,35 @@ package main
 
 import (
 	"fmt"
+
 	"time"
+
+	"github.com/henrique-mariano/learngo/functions"
 )
 
 var c, python, java bool //pode estar num pacote ou a nível de função.
 
 var k, j int = 1, 2 //a declaração pode incluir inicializadores, uma por variável
 
+// eoq := 12 //não é possível utilizar a atribuição curta fora do escopo de funções
+
 func main() {
 	fmt.Println("Vamos aprenderrr!!")
 
 	fmt.Println("A hora agora é:", time.Now())
 
-	printRand()
+	functions.PrintRand()
 
-	printProblems()
+	functions.PrintProblems()
 
-	exportedNames()
+	functions.ExportedNames()
 
-	fmt.Println("Soma:", add(25, 12))
+	fmt.Println("Soma:", functions.Add(25, 12))
 
-	a, b := swap("hello", "world")
+	a, b := functions.Swap("hello", "world")
 	fmt.Println("Swap:", a, b)
 
-	x, y := split(17)
+	x, y := functions.Split(17)
 	fmt.Println("Split:", x, y) //é possível só chamar Printl(split(17))
 
 	var i int //var declara uma lista de variáveis, como em listas de argumentos de função, o tipo é o último passado.
@@ -33,4 +38,7 @@ func main() {
 
 	var c, python, java = true, false, "no!" //se tiver um inicializador, o tipo pode ser omitido
 	fmt.Println(k, j, c, python, java)
+
+	complexodmais := 1 + 2i
+	fmt.Println(complexodmais)
 }
